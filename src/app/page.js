c0f7370c6524058606a64edab5e2c46bda7fca9a
joaocarloslima <joaocarloslima@me.com>
2023-08-25 09:35:09 -0300

    card filme

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,22 @@
+import CardFilme from "@/components/CardFilme";
 import Titulo from "@/components/Titulo";
 
 export default function Home() {
+  //mock
+  const filmes = [
+    {
+      titulo: "Star Wars",
+      nota: 9.5,
+      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/nNTvClgnd3FfQ45IVQkk4HLHuBN.jpg"
+    },
+    {
+      titulo: "Barbie",
+      nota: 9.0,
+      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+    }
+  ] 
+
+
   return (
     <>
       <nav className="bg-slate-900 p-4">
@@ -8,6 +24,11 @@ export default function Home() {
       </nav>
 
       <Titulo>em alta</Titulo>
+
+      <section className="flex flex-wrap gap-2">
+        {filmes.map( filme => <CardFilme filme={filme} /> )}
+      </section>
+
       <Titulo>lançamentos</Titulo>
 
     </>
